a16e6a81d044f7d4cd22fd917f8bdfab9e065a00
JonelSV <svjonel@gmail.com>
2021-10-22 21:05:32 -0400

    first commit

---
+++ src/main/java/com/capstonePractice/CapstonePracticeApplication.java
@@ -0,0 +1,14 @@
+package com.capstonePractice;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstonePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstonePractice/CapstonePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstonePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstonePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
